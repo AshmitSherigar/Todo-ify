b467395c2d91b36f65a52453d0efc3aa601da8f5
AshmitSherigar <sherigarashmit@gmail.com>
2025-07-29 22:01:34 +0530

    Ignore ENV files

--- backend-todo-app/server.js
+++ backend-todo-app/server.js
@@ -2,9 +2,10 @@
 const express = require("express")
 const cors = require("cors")
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const { createTodo, updateTodo } = require("./types")
 const { Todo } = require("./db")
+
 
 // Middleware
 app.use(express.json());
