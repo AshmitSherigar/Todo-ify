ad631834efda6c78761bfe80e8d278dcc648624b
AshmitSherigar <sherigarashmit@gmail.com>
2025-07-29 22:09:07 +0530

    v1 : minor error fixed

---
+++ backend-todo-app/db.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const mongoose = require("mongoose")
+mongoose.connect(process.env.MONGODB_URI)
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const Todo = mongoose.model("Todos", todoSchema)
+
+module.exports = { Todo }
